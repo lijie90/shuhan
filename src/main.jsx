92caf1a27323e4e7776ad8f21a196782865e5b3f
Ajay.Li（李杰） <ajay.li@feisu.com>
2026-06-12 14:47:44 +0800

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import { ThemeProvider } from './ThemeContext'
 import App from './App.jsx'
 import './index.css'
 
+const basename = import.meta.env.BASE_URL === '/' ? '' : import.meta.env.BASE_URL
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <ThemeProvider>
         <App />
       </ThemeProvider>
